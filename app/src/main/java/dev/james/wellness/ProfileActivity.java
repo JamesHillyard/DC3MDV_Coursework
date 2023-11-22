9d67df1f88a9733a8a97217de1ea9cdab62de0a8
James Hillyard <james.hillyard@payara.fish>
2023-11-22 19:28:22 +0000

    New Profile POC
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

--- app/src/main/java/dev/james/wellness/ProfileActivity.java
+++ app/src/main/java/dev/james/wellness/ProfileActivity.java
@@ -1,10 +1,12 @@
 package dev.james.wellness;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import dev.james.wellness.listener.SeekBarChangeListener;
 
@@ -14,20 +16,5 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
-
-        // Target Weight
-        SeekBar weightSlider = findViewById(R.id.weightSlider);
-        TextView weightValue = findViewById(R.id.weightValue);
-        SeekBarChangeListener.setupSeekBarChangeListener(weightSlider, weightValue, 1);
-
-        // Target Steps
-        SeekBar stepsSlider = findViewById(R.id.stepsSlider);
-        TextView stepsValue = findViewById(R.id.stepsValue);
-        SeekBarChangeListener.setupSeekBarChangeListener(stepsSlider, stepsValue, 250);
-
-        // Target Sleep
-        SeekBar sleepSlider = findViewById(R.id.sleepSlider);
-        TextView sleepValue = findViewById(R.id.sleepValue);
-        SeekBarChangeListener.setupSeekBarChangeListener(sleepSlider, sleepValue, 1);
     }
 }
